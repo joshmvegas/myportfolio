6e476fd13c8de5215157e62945938c5d8d94d7a1
davidvenegas <venegasdavidm@gmail.com>
2022-03-25 00:54:27 -0400

    don't kill me, I can explain just call me

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import { Fragment } from "react"
 import { Routes, Route } from "react-router-dom"
 import Navbar from './Navbar/Navbar'
 import About from './1-About/About'
-import Skills from './2-Skills/Skills'
-import Work from './3-Work/Work'
-import Algorithms from './4-Algorithms/Algorithms'
-import Blogs from './5-Blogs/Blogs'
-import Contact from './6-Contact/Contact'
 
 function App() {
   return (
@@ -14,11 +9,6 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<About/>} />
-        <Route path="/skills" element={<Skills/>} />
-        <Route path="/work" element={<Work/>} />
-        <Route path="/algorithms" element={<Algorithms/>} />
-        <Route path="/blogs" element={<Blogs/>} />
-        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </Fragment>
   )
