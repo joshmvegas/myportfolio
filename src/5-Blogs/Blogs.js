3945c1dfe433c3f3eb481a79e55a1e8055b9e513
davidvenegas <venegasdavidm@gmail.com>
2022-04-29 00:33:04 -0500

    alright, your turn jo

--- src/5-Blogs/Blogs.js
+++ src/5-Blogs/Blogs.js
@@ -2,8 +2,7 @@ import './blogs.css'
 import { useEffect, useState } from 'react'
 import ReactBanner from '../Images/blog-banners/react-banner.jpg'
 import JavascriptBanner from '../Images/blog-banners/javascript-banner.jpg'
-import RubyBanner from '../Images/blog-banners/ruby-banner.png'
-import MediumClap from '../Images/medium-clap.png'
+import RubyBanner from '../Images/blog-banners/ruby-banner.jpg'
 
 function Blogs() {
     useEffect(() => {window.scrollTo(0, 0)}, [])
@@ -28,13 +27,23 @@ function Blogs() {
     return (
         <div className='blogs-container'>
             <h1 className="blog-header">
-                <span>B</span>
-                <span>l</span>
+                <span>J</span>
                 <span>o</span>
-                <span>g</span>
+                <span>u</span>
+                <span>r</span>
+                <span>n</span>
+                <span>a</span>
+                <span>l</span>&nbsp;
+                <span>A</span>
+                <span>r</span>
+                <span>t</span>
+                <span>i</span>
+                <span>c</span>
+                <span>l</span>
+                <span>e</span>
                 <span>s</span>
             </h1>
-            <p className="blog-description">I enjoy keeping myself informed. Here's a few things I've written.</p>
+            <p className="blog-description">I constantly strive to keep myself informed. Here are a few things I've written.</p>
             <div className="blog-wrapper">
                 <input type="radio" name="slider" id="blog-item-1" defaultChecked/>
                 <input type="radio" name="slider" id="blog-item-2"/>
@@ -44,17 +53,16 @@ function Blogs() {
                         <div className="blog-content">
                             <img className='blog-image' src={ReactBanner} alt="React Banner" />
                             <div className="blog-text-content">
-                                <h1 className='blog-title'><a style={blog1 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-origin-of-react-db39d84c8503' target="_blank" rel="noreferrer">The Origin of React</a></h1>
+                                <h1 className='blog-title'><a style={blog1 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasjoshua1/comparing-the-effects-of-distracting-sensory-stimulation-on-auditory-perception-using-auditory-864b851ce4a5' target="_blank" rel="noreferrer">Distracting Stimulation on Auditory Perception</a></h1>
                                 <div className="blog-start-wrap">
-                                    <p className='blog-start'>Learn the history behind one of the world’s most popular programming libraries. React, (also known as ReactJS or React.js) is a popular free and open-source front-end Javascript library that is used for building UI components and user interfaces. It was created by Facebook and is still maintained by them along with an ever growing<span id='blogStartResponsive'> community of companies and individual developers. It is currently listed at or near the top of many industry rankings for popularity, developer</span>...&nbsp; <a style={blog1 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-origin-of-react-db39d84c8503' target="_blank" rel="noreferrer">Read More</a></p>
+                                    <p className='blog-start'>Auditory processing is an important physiological aspect with regard to human functioning. This processing can be hindered by distractions, which can occur in multiple forms. This study analyzed the effect of multiple types of secondary sensory distractions, including visual, cognitive, and somatic motor stimulation, on auditory<span id='blogStartResponsive'> perception. These effects were analyzed by measuring the widths of a university student’s auditory evoked potential waves collected during</span>...&nbsp; <a style={blog1 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasjoshua1/comparing-the-effects-of-distracting-sensory-stimulation-on-auditory-perception-using-auditory-864b851ce4a5' target="_blank" rel="noreferrer">Read More</a></p>
                                 </div>
                             </div>
                             <div className="blog-facts">
-                                <h4 className="blog-date">Nov 17, 2021</h4>
-                                <h2 className="blog-read">3 Minute Read</h2>
+                                <h4 className="blog-date">Jun 27th, 2021</h4>
+                                <h2 className="blog-read">11 Minute Read</h2>
                                 <div className="blog-likes">
-                                    <img src={MediumClap} alt="Claps" />
-                                    <p>104</p>
+                                    <p>ANATOMY</p>
                                 </div>
                             </div>
                         </div>
@@ -63,17 +71,16 @@ function Blogs() {
                         <div className="blog-content">
                             <img className='blog-image' src={JavascriptBanner} alt="Javascript Banner" />
                             <div className="blog-text-content">
-                                <h1 className='blog-title'><a style={blog2 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-origin-of-javascript-288154b923e' target="_blank" rel="noreferrer">The Origin Of JavaScript</a></h1>
+                                <h1 className='blog-title'><a style={blog2 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasjoshua1/covid-19-the-deadly-viruss-mechanism-epidemiology-transmission-prevention-and-prospective-b902729a27a' target="_blank" rel="noreferrer">COVID-19: The Virus’s Prospective Future</a></h1>
                                 <div className="blog-start-wrap">
-                                    <p className='blog-start'>In 1995 Brendan Eich, a Netscape Communications Corporation programmer, took 10 days to develop a scripting language he called Mocha. Back than internet connectivity used dial-up connections with telephone lines and modems, so when you filled out a form on a website — which was usually coded using a language like<span id='blogStartResponsive'> Perl — the validation of the input fields of any form had to make a full round trip to the server. This was not only very, very slow, but even could cause</span>...&nbsp; <a style={blog2 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-origin-of-javascript-288154b923e' target="_blank" rel="noreferrer">Read More</a></p>
+                                    <p className='blog-start'>Over a year after the world began reeling from the effects of a tiny intracellular parasite, multiple questions and ongoing misconceptions remain. How does the virus work, and how does it spread? What is the difference between the testing methods, and what exactly are they testing? What do we know about the<span id='blogStartResponsive'> disease this virus causes, and perhaps most importantly, what can we do to prevent it? This written work addresses each of these issues using recently</span>...&nbsp; <a style={blog2 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasjoshua1/covid-19-the-deadly-viruss-mechanism-epidemiology-transmission-prevention-and-prospective-b902729a27a' target="_blank" rel="noreferrer">Read More</a></p>
                                 </div>
                             </div>
                             <div className="blog-facts">
-                                <h4 className="blog-date">Oct 19, 2021</h4>
-                                <h2 className="blog-read">2 Minute Read</h2>
+                                <h4 className="blog-date">Apr 4th, 2021</h4>
+                                <h2 className="blog-read">23 Minute Read</h2>
                                 <div className="blog-likes">
-                                    <img src={MediumClap} alt="Claps" />
-                                    <p>57</p>
+                                    <p>IMMUNOLOGY</p>
                                 </div>
                             </div>
                         </div>
@@ -82,17 +89,16 @@ function Blogs() {
                         <div className="blog-content">
                             <img className='blog-image' src={RubyBanner} alt="Ruby Banner" />
                             <div className="blog-text-content">
-                                <h1 className='blog-title'><a style={blog3 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-best-ruby-enumerables-6d71fa5f933e' target="_blank" rel="noreferrer">The Best Ruby Enumerables</a></h1>
+                                <h1 className='blog-title'><a style={blog3 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-best-ruby-enumerables-6d71fa5f933e' target="_blank" rel="noreferrer">Electrical Activity in Antagonistic Muscles</a></h1>
                                 <div className="blog-start-wrap">
-                                    <p className='blog-start'><i>Here are a few of the best ruby enumerables and methods:</i> <br /> <b>all?</b> <br /> Return true when all elements passed into the block are true. <br /> <b>any?</b> <br /> Return true when all at least one element<span id='blogStartResponsive'> results in true when</span>...&nbsp; <a style={blog3 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-best-ruby-enumerables-6d71fa5f933e' target="_blank" rel="noreferrer">Read More</a></p>
+                                    <p className='blog-start'>Several electrodiagnostic tests are used to monitor and diagnose muscle abnormalities. Unfortunately, methods such as needle electromyography can be both invasive and painful. Surface electromyography, however, offers a less aggressive diagnostic alternative that can also be useful for<span id='blogStartResponsive'> detecting varying levels of activity in antagonistic skeletal muscles. In this experiment, two pairs of antagonistic muscles were monitored to assess the utility of</span>...&nbsp; <a style={blog3 ? null : {pointerEvents: "none"}} href='https://medium.com/@venegasdavidm/the-origin-of-javascript-288154b923e' target="_blank" rel="noreferrer">Read More</a></p>
                                 </div>
                             </div>
                             <div className="blog-facts">
                                 <h4 className="blog-date">Nov 23, 2021</h4>
-                                <h2 className="blog-read">2 Minute Read</h2>
+                                <h2 className="blog-read">13 Minute Read</h2>
                                 <div className="blog-likes">
-                                    <img src={MediumClap} alt="Claps" />
-                                    <p>64</p>
+                                    <p>BIOLOGY</p>
                                 </div>
                             </div>
                         </div>
